96342514326c2ef8d77ebb3d9bae44629cefe823
Belal <belalkoko00@gmail.com>
2025-08-01 00:00:20 +0300

    Update log.js

--- api/log.js
+++ api/log.js
@@ -213,6 +213,10 @@ function setSecurityHeaders(res) {
 
 // 🎯 Main Handler Function
 export default async function handler(req, res) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   try {
     // 🛡️ Set security headers
     setSecurityHeaders(res);
